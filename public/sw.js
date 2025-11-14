cb1be2d035b8fca8db79d6c13279decfc16a3975
Ahmed-ELnakieb <35165213+Ahmed-ELnakieb@users.noreply.github.com>
2025-11-14 15:16:45 +0200

    ok

---
+++ public/sw.js
@@ -0,0 +1,37 @@
+// Service Worker for PWA
+const CACHE_NAME = 'hokqq-v1';
+const urlsToCache = [
+  '/',
+  '/manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch from cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Update Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
